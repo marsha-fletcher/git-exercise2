6348fa2cc796bbb74446a3d1c0a6402a7d6fff16
Marsha Fletcher <fletcherml812@gmail.com>
2013-09-04 10:46:18 -0500

    Update HelloWorld.java

--- CommandLineGit/src/edu/grinnell/csc207/fletcher1/hello/HelloWorld.java
+++ CommandLineGit/src/edu/grinnell/csc207/fletcher1/hello/HelloWorld.java
@@ -3,6 +3,6 @@ package edu.grinnell.csc207.fletcher1.hello;
 public class HelloWorld {
     public static void main (String args[])
     {
-	System.out.println("Hello, GitHub!");
+	System.out.println("Hello, Octocat!");
     }
 }
